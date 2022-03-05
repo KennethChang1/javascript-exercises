66a2041631aa5fc116934e9ea2fc1029c82d7289
Kenneth <kenneth.kristopher27@gmail.com>
2022-03-05 17:17:37 +0700

    finish all test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split("");
+    let reverse = splitString.reverse();
+    let join = reverse.join("");
+    return join;
 };
 
 // Do not edit below this line
