fd20760117880a3c8cf5a34dae4ebfba10aae9fd
Kenneth <kenneth.kristopher27@gmail.com>
2022-03-06 16:55:37 +0700

    complete all test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...num) {
+    array = array.filter(val => !num.includes(val));
+    return array;
 };
 
 // Do not edit below this line
