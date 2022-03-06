9dbd59e220d41484e776cd485334da7b9003139a
Kenneth <kenneth.kristopher27@gmail.com>
2022-03-06 17:18:59 +0700

    completed all test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let c = 0;
+
+    if(a < 0 || b < 0 || typeof a !== "number" || typeof b !== "number") {
+        return "ERROR";
+    } else if(b > a && a >= 0) {
+        for(let i = 0; i < b; i++){
+            c += a;
+            a++;
+        }
+    } else if(a > b && b >= 0) {
+        for(let i = 0; i < a; i++){
+            c += b;
+            b++;
+        }
+    } 
+    return c;
 
 };
 
