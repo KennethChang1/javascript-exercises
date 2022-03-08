33d2dc8a8636eb7a35ee9cf23f7254f502d655ff
Kenneth <kenneth.kristopher27@gmail.com>
2022-03-08 13:05:22 +0700

    completed all test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    let str = a.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").toLowerCase();
+    str = str.replace(/\s/g, '');
+    let split = str.split("");
+    let reverse = split.reverse();
+    let join = reverse.join("");
+    if (str === join) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
