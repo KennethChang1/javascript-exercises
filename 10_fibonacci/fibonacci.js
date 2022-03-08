dda9ad5890a462411921d5a1ff627e0cd4db3804
Kenneth <kenneth.kristopher27@gmail.com>
2022-03-08 13:39:43 +0700

    completed all test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    let x = 0;
+    let y = 1;
+    let nextNum = 0;
+    if (a < 0) {
+        return "OOPS";
+    }
+    for (let i = 0; i < a; i++) {
+        nextNum = x + y;
+        x = y;
+        y = nextNum;
+    }
+    return x;
 };
 
 // Do not edit below this line
