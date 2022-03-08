4a37597c6826aa6e7094bd361f9ac58493c25fda
Kenneth <kenneth.kristopher27@gmail.com>
2022-03-08 11:13:29 +0700

    completed all test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let celcius;
+  celcius = (temp - 32) * 5/9;
+  celcius = Math.round(celcius * 10) / 10
+  return celcius;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let fahrenheit;
+  fahrenheit = temp * 9/5 + 32;
+  fahrenheit = Math.round(fahrenheit * 10) / 10
+  return fahrenheit;
 };
 
 // Do not edit below this line
