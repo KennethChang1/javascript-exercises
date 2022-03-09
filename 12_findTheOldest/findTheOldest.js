99e7dab0df6c52bdeecdf1911622d7bab2283955
Kenneth <kenneth.kristopher27@gmail.com>
2022-03-09 23:16:17 +0700

    completed all test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(a) {
+    const getAge = function(born, death) {
+        if (!death) {
+            death = new Date().getFullYear();
+        }
+        return death - born;
+    }
 
+    const old = a.reduce((oldest, currentValue) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentValue.yearOfBirth, currentValue.yearOfDeath);
+        if (oldestAge > currentAge) {
+            return oldest;
+        } else {
+            return currentValue;
+        }
+    })
+
+    return old;
 };
 
 // Do not edit below this line
